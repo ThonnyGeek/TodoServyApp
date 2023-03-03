6a4e00763af5319553f74bbea1434d38d4192568
Thonny <thonnygonz@gmail.com>
2023-03-03 11:42:43 -0500

    Initial Commit

---
+++ TodoServy App/TodoServy_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoServy_AppApp.swift
+//  TodoServy App
+//
+//  Created by Thony Gonzalez on 3/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoServy_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
